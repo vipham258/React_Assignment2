582d4e857b5fbcf29a890b33de5d4f98751dd014
Vi Pham <Vi.Pham@inspection.gc.ca>
2019-05-06 14:14:51 -0400

    Complete assignment2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import Validation from "./Validation/Validation";
+import Char from "./Char/Char";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" />
-      <div>
-        <input type="text" />
-        <p />
+class App extends Component {
+  state = {
+    userInput: ""
+  };
+
+  inputChangedHandler = event => {
+    this.setState({ userInput: event.target.value });
+  };
+
+  deleteCharHandler = index => {
+    const text = this.state.userInput.split("");
+    text.splice(index, 1);
+    const updatedText = text.join("");
+    this.setState({ userInput: updatedText });
+  };
+
+  render() {
+    const charList = this.state.userInput.split("").map((ch, index) => {
+      return (
+        <Char
+          character={ch}
+          key={index}
+          clicked={() => this.deleteCharHandler(index)}
+        />
+      );
+    });
+
+    return (
+      <div className="App">
+        <header className="App-header" />
+        <div>
+          <input
+            type="text"
+            onChange={this.inputChangedHandler}
+            value={this.state.userInput}
+          />
+          <p>{this.state.userInput}</p>
+          <Validation inputLength={this.state.userInput.length} />
+          {charList}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/Validation/Validation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const validation = props => {
+  let validationMessage = "Text long enough";
+  if (props.inputLength <= 5) {
+    validationMessage = "Text too short";
+  }
+
+  return <div>{validationMessage}</div>;
+};
+export default validation;
